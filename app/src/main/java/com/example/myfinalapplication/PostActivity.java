922f87012d823a272912b0059999ccbcaba4458e
“aaa” <b2394619748@163.com>
2023-06-06 17:16:54 +0800

    renew3

--- app/src/main/java/com/example/myfinalapplication/PostActivity.java
+++ app/src/main/java/com/example/myfinalapplication/PostActivity.java
@@ -7,10 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class PostActivity extends AppCompatActivity {
+    private EditText titleEditText = null;
+    private EditText contentEditText = null;
+    private String BoardId = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
